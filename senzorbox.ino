6a955fbcb00243014e0f1340567d2cc023c94597
Cosmin Albulescu <cosmin@albulescu.ro>
2015-03-01 00:54:50 +0200

    Fix timers

--- senzorbox.ino
+++ senzorbox.ino
@@ -5,19 +5,11 @@
 #include <Timer.h>
 #include <Event.h>
 
-#if defined(ARDUINO) && ARDUINO >= 100
-#include "Arduino.h"
-#else
-#include "WProgram.h"
-#endif
+int FREQUENCY = 1;
 
-int FREQUENCY = 20;
+unsigned long SEND_INTERVAL    = 300000;//5min
 
-// Send data to master interval
-int SEND_INTERVAL    = 1000 * 60;
-
-// set isMoving flag to false after this interval
-int MOVING_STOP_TIMEOUT  = 2000;
+int MOVING_STOP_TIMEOUT  = 3000;
 
 // Senzors zone
 int ZONE             = 4;
@@ -28,56 +20,48 @@ int TEMPERATURE_PIN = 1;
 int LIGHT_PIN       = A1;
 int HUMIDITY_PIN    = A3;
 int PIR_PIN         = A2;
-
+int LED_PIN         = A0;
 
 int PULSE_LENGTH     = 100;
 
 Timer timer;
 
 OneWire oneWire(TEMPERATURE_PIN);
- 
+
 // Pass our oneWire reference to Dallas Temperature.
 DallasTemperature sensors(&oneWire);
 
 // Keep last sent data
-// to check if send is 
+// to check if send is
 // required
 boolean sent_moving;
 int sent_temperature;
 int sent_luminosity;
 int sent_humidity;
-int sent_time;
+boolean isMoving = false;
 
-// PIR senzor is on
-boolean isMoving;
-
-int delayVal( int value ) {
-   return value / FREQUENCY;
+unsigned long delayVal( unsigned long value ) {
+  return value;
 }
 
 int read_temperature() {
-  
   sensors.requestTemperatures();
-  
-  int temperature = sensors.getTempCByIndex(0);
-  
-  
-  
-  return temperature;
-}  
+  return sensors.getTempCByIndex(0);
+}
 
 int read_humidity() {
-  return 50;
+  return analogRead(HUMIDITY_PIN)  * (99 / 1023.0);
 }
 
 int read_luminosity() {
-  return analogRead(LIGHT_PIN);
+  return analogRead(LIGHT_PIN)  * (99 / 1023.0);
 }
 
 boolean read_moving() {
   int voltage = analogRead(PIR_PIN)  * (5.0 / 1023.0);
-  return voltage > 1;
+  return voltage > 2;
 }
+
 
 char* dec2bin(unsigned long Dec, unsigned int bitLength){
   static char bin[64];
@@ -136,81 +120,72 @@ void sendBytes( char * sCodeWord ) {
   }
 }
 
-void blinkLed() {
-  
-  // Blink arduino led
-  digitalWrite(0, HIGH);
-  delay(delayVal(50));
-  digitalWrite(0, LOW);
-  delay(delayVal(50));
-  
-}
 
 void sendData() {
-  
+
   boolean moving  = read_moving();
   int temperature = read_temperature();
   int humidity    = read_humidity();
   int luminosity  = read_luminosity();
-  
-  if( temperature == sent_temperature &&
-      humidity == sent_humidity && 
-      moving == sent_moving &&
-      luminosity == sent_luminosity ) {
-      //return;
+
+  if ( temperature == sent_temperature &&
+       humidity == sent_humidity &&
+       moving == sent_moving &&
+       luminosity == sent_luminosity ) {
+    return;
   }
-  
+
   sent_temperature = temperature;
   sent_humidity = humidity;
   sent_luminosity = luminosity;
   sent_moving = moving;
-  
+
+  digitalWrite(LED_PIN, HIGH);
+ 
+  sendSync();
   sendBytes( dec2bin(ZONE, 4) );
   sendBytes( dec2bin(moving ? 1:0, 1) );
   sendBytes( dec2bin(temperature, 8) );
   sendBytes( dec2bin(humidity, 8) );
   sendBytes( dec2bin(luminosity, 8) );
-  sendSync();
-  
-  blinkLed();
-  
-  sent_time = millis();
+
+  digitalWrite(LED_PIN, LOW);
 
 }
 
 void setup() {
-  
-  timer.every(delayVal(SEND_INTERVAL), sendData);  
-  
+
   pinMode(EMIT_PIN, OUTPUT);
-  pinMode(TEMPERATURE_PIN, INPUT); 
+  pinMode(TEMPERATURE_PIN, INPUT);
   pinMode(LIGHT_PIN, INPUT);
   pinMode(HUMIDITY_PIN, INPUT);
   pinMode(PIR_PIN, INPUT);
-  
+  pinMode(LED_PIN, OUTPUT);
+
   sensors.begin();
+
+  timer.every(delayVal(SEND_INTERVAL), sendData);
 }
 
 void stopMoving() {
-   isMoving = false; 
-   blinkLed();
+  isMoving = false;
 }
 
 void loop() {
 
   boolean moving = read_moving();
-    
-  if( moving && moving != isMoving ) {
-    // if moving detected 
+
+  if ( moving && moving != isMoving ) {
+    // if moving detected
     // send data immediately
     sendData();
-    
+
     isMoving = moving;
-    
+
     timer.after(delayVal(MOVING_STOP_TIMEOUT), stopMoving);
   }
-  
+
   timer.update();
-  
-  delay(delayVal(100));
+
+  delay(delayVal(50));
 }
